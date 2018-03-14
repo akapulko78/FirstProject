b868281e77ec0530c0954a5650602f18bbd16b0c
akapulko78 <dgavrilodv@gmail.com>
2018-03-15 00:45:57 +0300

    Lesson3

--- src/Lesson3/Homework3.java
+++ src/Lesson3/Homework3.java
@@ -1,19 +1,37 @@
 package Lesson3;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Homework3 {
     public static void main(String[] args) {
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-        //Scanner sc = new Scanner(System.in);
-       // String b = sc.nextLine();
+
+        String str = "###############";
+        char[] charArray = str.toCharArray();
+
         Random rd = new Random();
         int index = rd.nextInt(words.length);
         String xword = words[index];
         System.out.println(xword);
+
+        Scanner scan = new Scanner(System.in);
+        String myword = scan.nextLine();
+
+
+        if (myword.equals(xword))
+            System.out.println("Right!");
+        else {
+            for (int i = 0; i < myword.length()||i<xword.length(); i++) {
+                if (xword.charAt(i) == myword.charAt(i))
+                    charArray[i] = myword.charAt(i);
+                }
+            for (int i = 0; i <charArray.length ; i++) {
+                System.out.println(charArray[i]);
+            }
+            }
+        }
     }
 
-
-}
 
---
+++ src/Lesson3/Homework3_1.java
@@ -0,0 +1,15 @@
+package Lesson3;
+
+import java.util.Random;
+
+public class Homework3_1 {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = i;
+            System.out.println(arr[i]);
+        }
+      //  Random rd = new Random();
+       // int index = rd.nextInt(arr.length);
+    }
+}
