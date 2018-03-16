41e258790ed1ce375f5af269c65e39fa2a14a40e
akapulko78 <dgavrilodv@gmail.com>
2018-03-16 17:25:14 +0300

    Lesson4 (code from lesson)

--- src/Lesson3/Homework3.java
+++ src/Lesson3/Homework3.java
@@ -6,32 +6,38 @@ import java.util.Scanner;
 
 public class Homework3 {
     public static void main(String[] args) {
-        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot",
+                "avocado", "broccoli", "carrot", "cherry", "garlic",
+                "grape", "melon", "leak", "kiwi", "mango", "mushroom",
+                "nut", "olive", "pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"};
 
-        String str = "###############";
-        char[] charArray = str.toCharArray();
+        char[] bars = new char[15]; //создаем массив символов
+        Arrays.fill(bars, '#'); //метод fill для заполнения массива одинаковыми элементами
 
         Random rd = new Random();
-        int index = rd.nextInt(words.length);
+        int index = rd.nextInt(words.length); //выбираем случайный элемент массива
         String xword = words[index];
-        System.out.println(xword);
+        //System.out.println(xword);
 
-        Scanner scan = new Scanner(System.in);
-        String myword = scan.nextLine();
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            String myword = scan.nextLine(); //считываем строку из консоли
 
-
-        if (myword.equals(xword))
-            System.out.println("Right!");
-        else {
-            for (int i = 0; i < myword.length()||i<xword.length(); i++) {
-                if (xword.charAt(i) == myword.charAt(i))
-                    charArray[i] = myword.charAt(i);
+            if (myword.equals(xword)) { //сравниваем слова
+                System.out.println("Right!");
+                break;
+            } else {
+                for (int i = 0; i < myword.length() && i < xword.length(); i++) {
+                    if (xword.charAt(i) == myword.charAt(i)) //сравниваем элементы массива под индексом i
+                        bars[i] = myword.charAt(i); //заменяем элемент массива bars элементом из массива myword
                 }
-            for (int i = 0; i <charArray.length ; i++) {
-                System.out.println(charArray[i]);
-            }
+                for (char bar : bars) { //цикл foreach для вывода в консоль массива bars
+                    System.out.print(bar);
+                }
             }
         }
     }
+}
 
 
